087776c7b5b1e6d6a945ed6a893c2a041d4a8e30
Ilayaraja Chandrasekaran <Ilayaraja84@gmail.com>
2018-08-17 12:53:38 -0400

    Initial Commit

---
+++ SurveyGizmoX.Tester/ParserTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SurveyGizmoX.Parser;
+namespace SurveyGizmoX.Tester
+{
+    [TestClass]
+    public class ParserTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var directory = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            var path = string.Format("{0}\\{1}", directory, "SampleData1.json");
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                SurveyGizmoResponse response = SurveyGizmoResponse.FromJson(json);
+                Assert.IsNotNull(response);
+
+
+            }
+        }
+    }
+}
---
+++ SurveyGizmoX/SurveyGizmoResponse.cs
@@ -0,0 +1,101 @@
+﻿// To parse this JSON data, add NuGet 'Newtonsoft.Json' then do:
+//
+//    using SurveyGizmoX.Parser;
+//
+//    var surveyGizmoResponse = SurveyGizmoResponse.FromJson(jsonString);
+
+namespace SurveyGizmoX.Parser
+{
+    using System;
+    using System.Collections.Generic;
+
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class SurveyGizmoResponse
+    {
+        [JsonProperty("is_test_data")]
+        public bool IsTestData { get; set; }
+
+        [JsonProperty("session_id")]
+        public string SessionId { get; set; }
+
+        [JsonProperty("language")]
+        public string Language { get; set; }
+
+        [JsonProperty("date_started")]
+        public string DateStarted { get; set; }
+
+        [JsonProperty("link_id")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long LinkId { get; set; }
+
+        [JsonProperty("url_variables")]
+        public Dictionary<string, UrlVariable> UrlVariables { get; set; }
+
+        [JsonProperty("ip_address")]
+        public string IpAddress { get; set; }
+
+        [JsonProperty("referer")]
+        public string Referer { get; set; }
+
+        [JsonProperty("user_agent")]
+        public string UserAgent { get; set; }
+
+        [JsonProperty("response_time")]
+        public long ResponseTime { get; set; }
+
+        [JsonProperty("data_quality")]
+        public object[] DataQuality { get; set; }
+
+        [JsonProperty("survey_data")]
+        public Dictionary<string, SurveyDatum> SurveyData { get; set; }
+    }
+
+    public partial class SurveyDatum
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("question")]
+        public string Question { get; set; }
+
+        [JsonProperty("section_id")]
+        public long SectionId { get; set; }
+
+        [JsonProperty("shown")]
+        public bool Shown { get; set; }
+
+        [JsonProperty("answer", NullValueHandling = NullValueHandling.Ignore)]
+        public string Answer { get; set; }
+
+        [JsonProperty("answer_id", NullValueHandling = NullValueHandling.Ignore)]
+        public long? AnswerId { get; set; }
+    }
+
+    public partial class UrlVariable
+    {
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public partial class SurveyGizmoResponse
+    {
+        public static SurveyGizmoResponse FromJson(string json) => JsonConvert.DeserializeObject<SurveyGizmoResponse>(json, SurveyGizmoX.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this SurveyGizmoResponse self) => JsonConvert.SerializeObject(self, SurveyGizmoX.Converter.Settings);
+    }
+}
